9609ca408870303ba7dc82b9057d9fe1023aee9b
Naveen Reddy <69370237+reddygn@users.noreply.github.com>
2023-10-12 21:19:33 -0500

    refactoring

--- employeemanagement-ui/src/layout/NavBar.js
+++ employeemanagement-ui/src/layout/NavBar.js
@@ -12,34 +12,14 @@ export default function Navbar() {
     <MDBNavbar dark bgColor='black'>
     <MDBContainer fluid>
       <MDBInputGroup tag="form" className='d-flex w-auto mb-3'>
-        {/* <input className='form-control' placeholder="Type query" aria-label="Search" type='Search' /> */}
-        {/* <MDBBtn outline>Search</MDBBtn> */}
-
-
-    
-      
-
-  
-
           <div >
-          <Link  style={{ color: "white",  
-                       textAlign: "start", fontSize:"40px"}} to="/">
+          <Link  style={{ color: "white", textAlign: "start", fontSize:"40px"}} to="/">
             Employees Management Applications
-          </Link>
-                  
+          </Link>  
           <Link style={{ color: "white",textAlign: "start", fontSize:"10px", marginTop:"20px", marginBottom:"14px", marginLeft:"966px"}} className="btn btn-outline-light" to="/add-employee">
             Add Employee
           </Link>
-          {/* <Link style={{ color: "white",textAlign: "start", fontSize:"10px", marginTop:"20px", marginBottom:"14px"}} className="btn btn-outline-light" to="/add-employee">
-            Search Employee
-          </Link> */}
-
           </div>
-      
-
-
-
-
       </MDBInputGroup>
     </MDBContainer>
   </MDBNavbar>
