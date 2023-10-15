d9316122c5bbf6e44729ef7c106d1a8ff27c905f
Naveen Reddy <69370237+reddygn@users.noreply.github.com>
2023-10-15 14:00:43 -0500

    Uncommenting the code to add employee on the UI - Home page

--- employeemanagement-ui/src/layout/NavBar.js
+++ employeemanagement-ui/src/layout/NavBar.js
@@ -16,9 +16,9 @@ export default function Navbar() {
           <Link  style={{ color: "white", textAlign: "start", fontSize:"40px"}} to="/">
             Employees Management Application
           </Link>  
-          {/* <Link style={{ color: "white",textAlign: "start", fontSize:"10px", marginTop:"20px", marginBottom:"14px", marginLeft:"966px"}} className="btn btn-outline-light" to="/add-employee">
+          <Link style={{ color: "white",textAlign: "start", fontSize:"10px", marginTop:"20px", marginBottom:"14px", marginLeft:"966px"}} className="btn btn-outline-light" to="/add-employee">
             Add Employee
-          </Link> */}
+          </Link>
           </div>
       </MDBInputGroup>
     </MDBContainer>
