a9f58739a790084cdbae7fd83138d24853959379
Naveen Reddy <69370237+reddygn@users.noreply.github.com>
2023-10-16 22:43:44 -0500

    Some more test cases in AssertsServiceTests.java

--- EmployeeManagementBackend/src/test/java/com/naveen/EmployeeManagement/service/AssetsServiceTests.java
+++ EmployeeManagementBackend/src/test/java/com/naveen/EmployeeManagement/service/AssetsServiceTests.java
@@ -1,9 +1,12 @@
 package com.naveen.EmployeeManagement.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Before;
@@ -38,14 +41,13 @@ public class AssetsServiceTests {
 		Mockito.when(assetsRepo.findById(1L)).thenReturn(a);
 
 		assertEquals(a.get().getAssetId(), "ABC");
-
 		assertEquals(a.get().getAssetName(), "QWERTY");
 
 	}
 
 	@Test
 	public void testGetAssetByIdFail() {
-		
+
 		Optional<Assets> a = Optional.of(new Assets());
 
 		a.get().setAssetId("ABC");
@@ -54,12 +56,50 @@ public class AssetsServiceTests {
 
 		Mockito.when(assetsRepo.findById(1L)).thenReturn(a);
 
-//        assertEquals(a.get().getEmployeeId(), 1L);
-
 		assertNotEquals(a.get().getAssetId(), "BBC");
-
 		assertNotEquals(a.get().getAssetName(), "LLC");
 
 	}
 
+	@Test
+	public void testGetEmployeeAssets() {
+
+		List<Assets> assets = new ArrayList<Assets>();
+
+		Assets a1 = new Assets();
+
+		a1.setAssetId("ABC");
+		a1.setAssetName("QWERTY");
+		a1.setEmployeeId(1L);
+
+		assets.add(a1);
+
+		Assets a2 = new Assets();
+		a2.setAssetId("CBA");
+		a2.setAssetName("QWERTY");
+		a2.setEmployeeId(1L);
+
+		assets.add(a2);
+
+		assertEquals(assets.size(), 2);
+		assertNotEquals(assets.size(), 4);
+	}
+
+	@Test
+	public void testAddAsset() {
+
+		Assets a = new Assets();
+
+		a.setAssetId("ABC");
+		a.setAssetName("QWERTY");
+		a.setEmployeeId(1L);
+
+		Mockito.when(assetsRepo.saveAndFlush(a)).thenReturn(a);
+
+		Assets returnedAsset = assetsRepo.saveAndFlush(a);
+
+		assertEquals("ABC", returnedAsset.getAssetId());
+		assertEquals("QWERTY", returnedAsset.getAssetName());
+
+	}
 }
